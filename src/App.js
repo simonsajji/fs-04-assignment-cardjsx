35293047bfd6f4b6881dc3a633e065b71823ceee
simonsajjizoft <simon.sajimathews@zoftsolutions.com>
2023-03-16 18:41:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import {React,createElement} from 'react';
 
 function App() {
+  let element = createElement(
+    'div',{ className: 'greeting', },
+    createElement('img',{src:"https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"}),
+    createElement('button',{className:"plus-button" },createElement('i',{className:"fa fa-plus plus-icon", })),
+    createElement('i',{className:"fa fa-ellipsis-v edit-icon", }),
+    createElement('div',{className:"profile"},createElement('h2',{className:"name"},"Dima Bolver"),createElement('p',{className:"desig"},"UI/UX Designer"),
+    createElement('div',{className:"sections"},
+    createElement('div',null,createElement('div',null,createElement("div",{className:'number'},'26'),createElement('div',null,createElement("div",{className:'label'},'Shoots')))),
+    createElement('div',null,createElement('div',null,createElement("div",{className:'number'},'98'),createElement('div',null,createElement("div",{className:'label'},'Following')))),
+    createElement('div',null,createElement('div',null,createElement("div",{className:'number'},'129'),createElement('div',null,createElement("div",{className:'label'},'Followers')))),
+    )),
+    
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="greeting">
+      {element}
+    
     </div>
   );
 }
